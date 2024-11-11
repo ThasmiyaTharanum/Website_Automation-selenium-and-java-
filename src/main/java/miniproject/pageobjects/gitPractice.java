7ae98f8680effac3120a49e0e9e5df2c39ac2ed4
Thasmiya Tharanum <thasmiyatharanum@gmail.com>
2024-11-11 19:54:47 +0530

    Thasmiya commited

--- src/main/java/miniproject/pageobjects/gitPractice.java
+++ src/main/java/miniproject/pageobjects/gitPractice.java
@@ -8,6 +8,8 @@ public class gitPractice {
 		System.out.println("Welcome to git");
 		System.out.println("Welcome to git1");
 		System.out.println("Welcome to git2");
+		System.out.println("Welcome to git4");
+		System.out.println("Welcome to git5");
 	}
 
 }
